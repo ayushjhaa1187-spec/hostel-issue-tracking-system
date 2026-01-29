fa184e4d01d9f6215d389bf141c4f8c0f071ddce
AYUSH KUMAR JHA <ayushjhaa1187@gmail.com>
2026-01-29 08:27:07 +0530

    Create issues.js

---
+++ backend/routes/issues.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+  createIssue,
+  getIssues,
+  updateIssueStatus,
+  addComment,
+  upvoteIssue,
+  assignIssue
+} = require('../controllers/issueController');
+const { protect, authorize } = require('../middleware/auth');
+
+router.route('/')
+  .post(protect, createIssue)
+  .get(protect, getIssues);
+
+router.put('/:id/status', protect, authorize('staff', 'admin'), updateIssueStatus);
+router.post('/:id/comments', protect, addComment);
+router.put('/:id/upvote', protect, upvoteIssue);
+router.put('/:id/assign', protect, authorize('admin'), assignIssue);
+
+module.exports = router;
